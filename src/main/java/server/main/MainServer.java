0db51aa66d827be405819be6eec1875ca88f5949
Andrew <andrewkennedyftw@gmail.com>
2015-10-08 14:23:40 -0500

    worked on ClientData a tiny bit

--- src/main/java/server/main/MainServer.java
+++ src/main/java/server/main/MainServer.java
@@ -3,11 +3,12 @@ package main;
 import java.io.IOException;
 
 import server.Server;
+import utils.ConfigManager;
 
 public class MainServer {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		try {
-			Server s = new Server(25565);
+			Server s = new Server(ConfigManager.getInt("SERVER_PORT"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
