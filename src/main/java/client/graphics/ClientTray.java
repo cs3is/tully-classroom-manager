7552ed835b0decbfa872d599e37d9bc1ed91e86c
Andrew <andrewkennedyftw@gmail.com>
2015-10-20 13:51:27 -0500

    fixed an issue where log was not being imprted in clietTray

--- src/main/java/client/graphics/ClientTray.java
+++ src/main/java/client/graphics/ClientTray.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 
 import utils.ClientConfig;
 import utils.ClientConfigManager;
+import utils.Log;
 import utils.ServerLog;
 
 public class ClientTray implements Runnable {
