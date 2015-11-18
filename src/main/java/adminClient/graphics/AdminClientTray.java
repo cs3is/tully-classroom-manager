b050ff3f0cef091f14886ef687cdff9f5b540e47
Bo Corman <bo.corman@gmail.com>
2015-11-18 13:58:52 -0600

    made tully go away when left clicked (admin client) (will later be used to opena  frame)

--- src/main/java/adminClient/graphics/AdminClientTray.java
+++ src/main/java/adminClient/graphics/AdminClientTray.java
@@ -9,6 +9,8 @@ import java.awt.Toolkit;
 import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 import client.ClientData;
@@ -81,19 +83,15 @@ public class AdminClientTray implements Runnable{
 			}
 		});
 		
-		trayIcon.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
-		
-//		trayIcon.addMouseListener(new MouseAdapter() {
-//		    public void mouseClicked(MouseEvent e) {
-//		        if (e.getClickCount() == 1) {
-//
-//		        }
-//		    }
-//		}); 
+		trayIcon.addMouseListener(new MouseAdapter() {
+		    public void mouseClicked(MouseEvent e) {
+		        if (e.getClickCount() == 1) {
+		        	if(e.getButton() == e.BUTTON1){
+		        		System.exit(0);
+		        	}
+		        }
+		    }
+		}); 
 			
 
 	}
